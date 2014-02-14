25183f7f06989795c6c71ceb8816b38480763f2b
frenesim <hlsp999@gmail.com>
2014-02-14 18:39:12 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frambo::Application.config.secret_token = '88c78348afa5402ac17a2a7cde20d2777559f003a0a29a5018fe53a75481a9df73c7471bbb461d1670f4916329e39c134a21ffa1199ec2d79407d0f83a2f059b'
